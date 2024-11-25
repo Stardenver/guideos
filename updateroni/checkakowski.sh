90cafb9c7e7c59bbe3c7c647100fccad79c04d84
Boris Schäfer <65340721+Stardenver@users.noreply.github.com>
2024-11-25 17:31:00 +0100

    Update checkakowski.sh

--- updateroni/checkakowski.sh
+++ updateroni/checkakowski.sh
@@ -3,10 +3,13 @@
 # Aktualisiere die Paketliste
 sudo apt update > /dev/null 2>&1
 
-# Überprüfe auf verfügbare Upgrades
+# \u00dcberpr\u00fcfe auf verf\u00fcgbare Upgrades
 updates=$(apt list --upgradable 2>/dev/null | grep -v "Listing..." | wc -l)
 
 # Wenn es Updates gibt, sende eine Benachrichtigung
 if [ "$updates" -gt 0 ]; then
-  notify-send "Updates verfügbar" "Es gibt $updates Updates für GuideOS. Bitte installieren Sie diese bei Gelegenheit mit 'sudo apt upgrade'."
+  notify-send "Updates verf\u00fcgbar" "Es gibt $updates Updates f\u00fcr GuideOS. Bitte starte das Update-Tool."
 fi
+
+
+
